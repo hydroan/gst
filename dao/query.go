4c3235086a82a2aaf8246e452b654be69226336a
hydroan <loganhydro863@gmail.com>
2026-06-23 17:58:43 +0800

    feat(dao): expose query map helpers
    Move the query-to-map helper from internal dao code to the public dao package and add a configurable variant for callers that need custom query options.

---
+++ dao/query.go
@@ -0,0 +1,38 @@
+package dao
+
+import (
+	"github.com/hydroan/gst/database"
+	"github.com/hydroan/gst/types"
+)
+
+func QueryModelsMap[M types.Model](ctx *types.DatabaseContext, keyFunc func(M) string, queryFunc func() M) (map[string]M, error) {
+	return QueryModelsMapWithConfig[M](ctx, keyFunc, queryFunc, types.QueryConfig{AllowEmpty: true})
+}
+
+func QueryModelsMapWithConfig[M types.Model](ctx *types.DatabaseContext, keyFunc func(M) string, queryFunc func() M, config types.QueryConfig) (map[string]M, error) {
+	if keyFunc == nil {
+		keyFunc = func(m M) string {
+			return m.GetID()
+		}
+	}
+	if queryFunc == nil {
+		queryFunc = func() M {
+			var m M
+			return m
+		}
+	}
+
+	objs := make([]M, 0)
+	if err := database.Database[M](ctx).
+		WithQuery(queryFunc(), config).
+		List(&objs); err != nil {
+		return nil, err
+	}
+
+	objMap := make(map[string]M)
+	for _, obj := range objs {
+		objMap[keyFunc(obj)] = obj
+	}
+
+	return objMap, nil
+}
