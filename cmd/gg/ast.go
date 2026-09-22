f3c8231ebde7266be85036ed983a6a64946a2698
hydroan <loganhydro863@gmail.com>
2026-09-22 20:32:42 +0800

    refactor(gg): drop the ast subcommands that did nothing
    gg ast go2json and gg ast json2go were registered with empty bodies: they accepted a file and printed nothing. gg ast dump stays.

--- cmd/gg/ast.go
+++ cmd/gg/ast.go
@@ -30,24 +30,8 @@ var astdumpCmd = &cobra.Command{
 	},
 }
 
-var astgo2jsonCmd = &cobra.Command{
-	Use:   "go2json",
-	Short: "convert go file to json",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
-
-var astjson2goCmd = &cobra.Command{
-	Use:   "json2go",
-	Short: "convert json to go file",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
-
 func init() {
 	astdumpCmd.Flags().BoolVarP(&ispretty, "pretty", "p", false, "enable pretty print")
 
-	astCmd.AddCommand(astdumpCmd, astgo2jsonCmd, astjson2goCmd)
+	astCmd.AddCommand(astdumpCmd)
 }
